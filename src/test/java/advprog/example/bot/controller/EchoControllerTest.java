2c2b033c18c4893817b84b89e377db2c0f132f8a
Daya Adianto <daya.adianto@gmail.com>
2018-05-09 08:38:40 +0000

    Prepare CI & finalise template

--- src/test/java/advprog/example/bot/controller/EchoControllerTest.java
+++ src/test/java/advprog/example/bot/controller/EchoControllerTest.java
@@ -7,6 +7,8 @@ import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import advprog.example.bot.EventTestUtil;
+
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
@@ -18,8 +20,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import advprog.example.bot.EventTestUtil;
 
 @SpringBootTest(properties = "line.bot.handler.enabled=false")
 @ExtendWith(SpringExtension.class)
@@ -41,7 +41,7 @@ public class EchoControllerTest {
     @Test
     void testHandleTextMessageEvent() {
         MessageEvent<TextMessageContent> event =
-                EventTestUtil.createDummyTextMessage("Lorem Ipsum");
+                EventTestUtil.createDummyTextMessage("/echo Lorem Ipsum");
 
         TextMessage reply = echoController.handleTextMessageEvent(event);
 
