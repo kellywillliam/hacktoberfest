ec84da2be4fe167723cb046584dab1b49a1d21bf
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-05-09 07:54:54 +0700

    Initial commit

---
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -0,0 +1,29 @@
+package advprog.example.bot.controller;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import java.util.logging.Logger;
+
+@LineMessageHandler
+public class EchoController {
+
+    private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
+                event.getTimestamp(), event.getMessage()));
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessage(Event event) {
+        LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
+                event.getTimestamp(), event.getSource()));
+    }
+}
