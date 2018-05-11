bcc00a232cccaec0590780d78bd82c1ef9c111be
kevinprakasa <kevinprakasa03@gmail.com>
2018-05-11 11:21:19 +0700

    add  unit test for youtube scrapping controller

---
+++ src/test/java/advprog/example/bot/controller/YoutubeScrappingControllerTest.java
@@ -0,0 +1,46 @@
+package advprog.example.bot.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import advprog.example.bot.EventTestUtil;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest(properties = "line.bot.handler.enabled=false")
+@ExtendWith(SpringExtension.class)
+public class YoutubeScrappingControllerTest {
+
+    static {
+        System.setProperty("line.bot.channelSecret", "SECRET");
+        System.setProperty("line.bot.channelToken", "TOKEN");
+    }
+
+    @Autowired
+    private YoutubeScrappingController youtubeScrappingController;
+
+    @Test
+    void testContextLoads() {
+        assertNotNull(youtubeScrappingController);
+    }
+
+    @Test
+    void testHandleTextMessageEvent() {
+
+        assertEquals("title Youtube Video", "title Youtube Video"));
+    }
+}
