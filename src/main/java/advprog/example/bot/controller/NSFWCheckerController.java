c873f9ff964d00e3d0a821e29eef77eabe5668ef
Ignatius <ignatusrahardi@gmail.com>
2018-05-11 11:43:56 +0700

    pushing branch and added early test for sfw checking

---
+++ src/main/java/advprog/example/bot/controller/NSFWCheckerController.java
@@ -0,0 +1,32 @@
+package advprog.example.bot.controller;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+
+import java.util.logging.Logger;
+
+public class NSFWCheckerController {
+
+    private static final Logger LOGGER = Logger.getLogger(NSFWCheckerController.class.getName());
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
+                event.getTimestamp(), event.getMessage()));
+        TextMessageContent content = event.getMessage();
+        String contentText = content.getText();
+
+        String replyText = contentText.replace("/is_sfw", "");
+        return new TextMessage(replyText.substring(1));
+    }
+
+    @EventMapping
+    public void handleDefaultMessage(Event event) {
+        LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
+                event.getTimestamp(), event.getSource()));
+    }
+
+}
