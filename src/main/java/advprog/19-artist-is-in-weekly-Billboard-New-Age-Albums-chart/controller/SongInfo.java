7b7072bd0d84e3b05f45b69c0115436d5944b611
Kezia Irene <irenekezia98@gmail.com>
2018-05-11 20:57:53 +0700

    make tests and app

---
+++ src/main/java/advprog/19-artist-is-in-weekly-Billboard-New-Age-Albums-chart/controller/SongInfo.java
@@ -0,0 +1,25 @@
+package advprog.hot;
+
+public class SongInfo {
+    private String songArtist;
+    private String songTitle;
+    private int rank;
+
+    public SongInfo(String songArtist, String songTitle, int rank) {
+        this.songArtist = songArtist;
+        this.songTitle = songTitle;
+        this.rank = rank;
+    }
+
+    public String getSongArtist() {
+        return this.songArtist;
+    }
+
+    public String getSongTitle() {
+        return this.songTitle;
+    }
+
+    public int getRank() {
+        return this.rank;
+    }
+}
