93626381766498d17891f51718ced26a9470e6d7
= <wehantouw28@yahoo.com>
2018-05-11 15:16:38 +0700

    Created a functional MusicBrainzController and edited MusicBrainzControllerTest

--- src/main/java/advprog/example/bot/controller/MusicBrainzController.java
+++ src/main/java/advprog/example/bot/controller/MusicBrainzController.java
@@ -1,14 +1,23 @@
 package advprog.example.bot.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.musicbrainz.MBWS2Exception;
+import org.musicbrainz.controller.Artist;
+import org.musicbrainz.model.entity.ArtistWs2;
+import org.musicbrainz.model.entity.ReleaseGroupWs2;
+import org.musicbrainz.model.searchresult.ArtistResultWs2;
+
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-import 
-
-import java.util.logging.Logger;
 
 @LineMessageHandler
 public class MusicBrainzController {
@@ -16,9 +25,9 @@ public class MusicBrainzController {
     private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
 
     @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-//        LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
-//                event.getTimestamp(), event.getMessage()));
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws MBWS2Exception {
+        LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
+                event.getTimestamp(), event.getMessage()));
 //        TextMessageContent content = event.getMessage();
 //        String contentText = content.getText();
 //        String replyText = contentText.replace("/echo", "");
@@ -28,10 +37,58 @@ public class MusicBrainzController {
         String contentText = content.getText();
 //        String[] splitContentText = contentText.split(" ");
         String artistName = contentText.replace("/10albums ", "");
-        return new TextMessage(artistName);
-        
+        return new TextMessage(albumSearch(artistName));
     }
 
+    public String albumSearch(String artistName) throws MBWS2Exception {
+    	String res = "";
+
+    	Artist artistSearch = new Artist();
+    	artistSearch.search(artistName);
+
+    	List<ArtistResultWs2> result = artistSearch.getFullSearchResultList();
+    	ArtistWs2 artist = new ArtistWs2();
+
+    	for (ArtistResultWs2 x : result) {
+    		if (x.getArtist().getName().equals(artistName)) {
+    			artist = x.getArtist();
+    		}
+    	}
+
+    	artistSearch = new Artist();
+    	artistSearch.lookUp(artist);
+
+    	List<ReleaseGroupWs2> release_groups = artistSearch.getFullReleaseGroupList();
+    	List<ReleaseGroupWs2> albums = new ArrayList<>();
+
+    	for (ReleaseGroupWs2 x : release_groups) {
+    
+    		if (x.getTypeString().equals("Album")) {
+    			albums.add(x);
+    			if (albums.size() == 10) {
+    				break;
+    			}
+    		}
+    	}
+    	
+    	Collections.sort(albums, new Comparator<ReleaseGroupWs2>() {
+    		
+    		@Override
+    		public int compare(ReleaseGroupWs2 o1, ReleaseGroupWs2 o2) {
+    			return o2.getYear().compareTo(o1.getYear());
+    		}
+    	});
+    	
+    	for (ReleaseGroupWs2 x : albums) {
+    		res+= artistName + "-" + x.getTitle() + "-" + x.getYear() + "\n";
+    	}
+    	
+    	if (res.length() == 0) {
+    		return "Sorry, we cannot find " + artistName + "'s album";
+    	}
+    	return res.substring(0, res.length() - 1);
+    }
+    
     @EventMapping
     public void handleDefaultMessage(Event event) {
         LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
--- src/test/java/advprog/example/bot/controller/MusicBrainzControllerTest.java
+++ src/test/java/advprog/example/bot/controller/MusicBrainzControllerTest.java
@@ -2,24 +2,23 @@ package advprog.example.bot.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-import advprog.example.bot.EventTestUtil;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.musicbrainz.MBWS2Exception;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import advprog.example.bot.EventTestUtil;
 
 @SpringBootTest(properties = "line.bot.handler.enabled=false")
 @ExtendWith(SpringExtension.class)
@@ -39,18 +38,15 @@ public class MusicBrainzControllerTest {
     }
 
     @Test
-    void testHandleTextMessageEvent() {
+    void testHandleTextMessageEvent() throws MBWS2Exception {
         MessageEvent<TextMessageContent> event =
-                EventTestUtil.createDummyTextMessage("/10albums Fall Out Boy");
+                EventTestUtil.createDummyTextMessage("/10albums Panic! at the Disco");
 
         TextMessage reply = musicBrainzController.handleTextMessageEvent(event);
 
-        //assertEquals("Lorem Ipsum", reply.getText());
         //System.out.println(reply.getText());
-        assertEquals("Fall Out Boy-M A N I A-2018\nFall Out Boy-American Beauty/American Psycho-2015\n"
-        		+ "Fall Out Boy-Save Rock and Roll-2013\nFall Out Boy-Folie à Deux-2008\n"
-        		+ "Fall Out Boy-Infinity On High-2007\nFall Out Boy-From Under The Cork Tree-2005\n"
-        		+ "Fall Out Boy-Take This to Your Grave-2003", reply.getText());
+        String[] lines = reply.getText().split("\n");
+        assertEquals(lines.length, 6);
     }
 
     @Test
