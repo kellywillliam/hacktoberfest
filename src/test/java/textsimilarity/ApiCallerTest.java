e70520cda09f58c67b82033cec974b8a8fd96c4e
lowellHS <lowell.sianipar@gmail.com>
2018-05-11 11:12:23 +0700

    Menambahkan partial unittests untuk kelas ApiCaller.java

---
+++ src/test/java/textsimilarity/ApiCallerTest.java
@@ -0,0 +1,25 @@
+package textsimilarity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ApiCallerTest {
+
+    private ApiCaller apicaller;
+
+    @Before
+    public void setUp() {
+        apicaller = new ApiCaller();
+    }
+
+    @Test
+    public void testTextsimilarity() {
+
+    }
+
+    @Test
+    public void testUrlsimilarity() {
+
+    }
+}
