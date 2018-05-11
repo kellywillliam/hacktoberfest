265ce9f7a1cff1930173d36447814f0dc7987291
arielmikiabraham <arielabraham82@gmail.com>
2018-05-11 20:31:52 +0700

    Refactor: Move stubs method to another package

---
+++ src/main/java/advprog/example/bot/sentiment/analysis/SentimentAnalysis.java
@@ -0,0 +1,10 @@
+package advprog.example.bot.sentiment.analysis;
+
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+
+public class SentimentAnalysis {
+    public static TextMessage analyzeText(String text) {
+        return null;
+    }
+}
