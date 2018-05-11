27826c13cf5b226d3ecc74e26a8142f45ffc8420
Azhar Kurnia <azharkurnia19@gmail.com>
2018-05-11 11:45:36 +0700

    add example bot (issue #20)

--- src/main/java/advprog/example/bot/BotExampleApplication.java
+++ src/main/java/advprog/example/bot/BotExampleApplication.java
@@ -8,5 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BotExampleApplication {
 
+    private static final Logger LOGGER = Logger.getLogger(BotExampleApplication.class.getName());
 
+    public static void main(String[] args) {
+        LOGGER.info("Application starting ...");
+        SpringApplication.run(BotExampleApplication.class, args);
+    }
 }
--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -12,5 +12,22 @@ import java.util.logging.Logger;
 @LineMessageHandler
 public class EchoController {
 
+    private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
 
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
+                event.getTimestamp(), event.getMessage()));
+        TextMessageContent content = event.getMessage();
+        String contentText = content.getText();
+
+        String replyText = contentText.replace("/echo", "");
+        return new TextMessage(replyText.substring(1));
+    }
+
+    @EventMapping
+    public void handleDefaultMessage(Event event) {
+        LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
+                event.getTimestamp(), event.getSource()));
+    }
 }
