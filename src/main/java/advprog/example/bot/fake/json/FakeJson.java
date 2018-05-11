141eb2a7de15ca32786d655f712c08261260066c
arielmikiabraham <arielabraham82@gmail.com>
2018-05-11 11:26:32 +0700

    Create stubs method

---
+++ src/main/java/advprog/example/bot/fake/json/FakeJson.java
@@ -0,0 +1,11 @@
+package advprog.example.bot.fake.json;
+
+import com.linecorp.bot.model.message.TextMessage;
+import org.springframework.web.client.RestTemplate;
+
+public class FakeJson {
+    public static TextMessage getFakeJson() {
+        return null;
+    }
+
+}
