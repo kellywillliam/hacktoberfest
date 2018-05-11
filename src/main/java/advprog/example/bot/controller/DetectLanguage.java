c671976b9fff27a7a38fe81eee88041057e8f89a
kevinprakasa <kevinprakasa03@gmail.com>
2018-05-11 20:42:11 +0700

    add stubs for detect languange

---
+++ src/main/java/advprog/example/bot/controller/DetectLanguage.java
@@ -0,0 +1,7 @@
+package advprog.example.bot.controller;
+
+public class DetectLanguage {
+    public String detectLang(String text) {
+        return "en 0.3423";
+    }
+}
---
+++ src/test/java/advprog/example/bot/controller/DetectLanguangeTest.java
@@ -0,0 +1,4 @@
+package advprog.example.bot.controller;
+
+public class DetectLanguangeTest {
+}
