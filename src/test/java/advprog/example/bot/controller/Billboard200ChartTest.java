93c7ab7fc190825abc702db32b981c4f1524712c
kevinprakasa <kevinprakasa03@gmail.com>
2018-05-12 22:41:22 +0700

    write unit test for stub

--- src/test/java/advprog/example/bot/controller/Billboard200ChartTest.java
+++ src/test/java/advprog/example/bot/controller/Billboard200ChartTest.java
@@ -1,4 +1,30 @@
 package advprog.example.bot.controller;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(properties = "line.bot.handler.enabled=false")
+@ExtendWith(SpringExtension.class)
 public class Billboard200ChartTest {
+
+    static {
+        System.setProperty("line.bot.channelSecret", "SECRET");
+        System.setProperty("line.bot.channelToken", "TOKEN");
+    }
+
+    @Test
+    void testContextLoads() {
+        assertNotNull(Billboard200Chart.class);
+    }
+
+    @Test
+    void testTop10Tracks() {
+        assertEquals("A-C", Billboard200Chart.top10Tracks());
+    }
+
 }
