d385d7dda33ff3e8f2a5571951d103f4da69bd8d
kevinprakasa <kevinprakasa03@gmail.com>
2018-05-12 22:41:07 +0700

    add stub for Top ten Tracks on billboard

---
+++ src/main/java/advprog/example/bot/controller/Billboard200Chart.java
@@ -0,0 +1,9 @@
+package advprog.example.bot.controller;
+
+public class Billboard200Chart {
+
+    private final static String url = "https://www.billboard.com/charts/billboard-200";
+    public static String top10Tracks() {
+        return "A-B";
+    }
+}
