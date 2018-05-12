e8652f610b8788dc626dfca97be27323102e4970
Misael Jonathan <misaeljonathan17@gmail.com>
2018-05-12 19:50:49 +0700

    Added Access to Line Bot, Access to Computer Vision API, (OW) obtain img from user

--- src/main/java/advprog/example/bot/controller/HandwrittenController.java
+++ src/main/java/advprog/example/bot/controller/HandwrittenController.java
@@ -7,12 +7,25 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.logging.Logger;
 
 @LineMessageHandler
 public class HandwrittenController {
 
-    private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(HandwrittenController.class.getName());
 
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
@@ -20,6 +33,10 @@ public class HandwrittenController {
                 event.getTimestamp(), event.getMessage()));
         TextMessageContent content = event.getMessage();
         String contentText = content.getText();
+
+        if (contentText.equalsIgnoreCase("cek foto")){
+            return new TextMessage("nih gue kasi textnya: ");
+        }
 
         String replyText = contentText.replace("/echo", "");
         return new TextMessage(replyText.substring(1));
@@ -30,7 +47,7 @@ public class HandwrittenController {
         LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
                 event.getTimestamp(), event.getSource()));
     }
-    
+
     public String convertHandwritten(){
         String convertedText = null;
         return "ini loh tulisannya: " + convertedText;
@@ -38,5 +55,118 @@ public class HandwrittenController {
 
     public String accessAPI(){
         return "";
+    }
+
+    // **********************************************
+    // *** Update or verify the following values. ***
+    // **********************************************
+
+    // Replace the subscriptionKey string value with your valid subscription key.
+    public static final String subscriptionKey = "353361421dc0412bbc1641a0c7824f0f";
+
+    // Replace or verify the region.
+    //
+    // You must use the same region in your REST API call as you used to obtain your subscription keys.
+    // For example, if you obtained your subscription keys from the westus region, replace
+    // "westcentralus" in the URI below with "westus".
+    //
+    // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+    // a free trial subscription key, you should not need to change this region.
+    //
+    // Also, for printed text, set "handwriting" to false.
+    public static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/recognizeText?handwriting=true";
+//    public static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0";
+
+    public static void main(String[] args)
+    {
+        HttpClient textClient = new DefaultHttpClient();
+        HttpClient resultClient = new DefaultHttpClient();
+
+        try
+        {
+            // This operation requrires two REST API calls. One to submit the image for processing,
+            // the other to retrieve the text found in the image.
+            //
+            // Begin the REST API call to submit the image for processing.
+            URI uri = new URI(uriBase);
+            HttpPost textRequest = new HttpPost(uri);
+
+            // Request headers. Another valid content type is "application/octet-stream".
+            textRequest.setHeader("Content-Type", "application/json");
+            textRequest.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity requestEntity =
+                    new StringEntity("{\"url\":\"https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Cursive_Writing_on_Notebook_paper.jpg/800px-Cursive_Writing_on_Notebook_paper.jpg\"}");
+            textRequest.setEntity(requestEntity);
+
+            // Execute the first REST API call to detect the text.
+            HttpResponse textResponse = textClient.execute(textRequest);
+
+            // Check for success.
+            if (textResponse.getStatusLine().getStatusCode() != 202)
+            {
+                // Format and display the JSON error message.
+                HttpEntity entity = textResponse.getEntity();
+                String jsonString = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("Error:\n");
+                System.out.println(json.toString(2));
+                return;
+            }
+
+            String operationLocation = null;
+
+            // The 'Operation-Location' in the response contains the URI to retrieve the recognized text.
+            Header[] responseHeaders = textResponse.getAllHeaders();
+            for(Header header : responseHeaders) {
+                if(header.getName().equals("Operation-Location"))
+                {
+                    // This string is the URI where you can get the text recognition operation result.
+                    operationLocation = header.getValue();
+                    break;
+                }
+            }
+
+            // NOTE: The response may not be immediately available. Handwriting recognition is an
+            // async operation that can take a variable amount of time depending on the length
+            // of the text you want to recognize. You may need to wait or retry this operation.
+
+            System.out.println("\nHandwritten text submitted. Waiting 10 seconds to retrieve the recognized text.\n");
+            Thread.sleep(10000);
+
+            // Execute the second REST API call and get the response.
+            HttpGet resultRequest = new HttpGet(operationLocation);
+            resultRequest.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            HttpResponse resultResponse = resultClient.execute(resultRequest);
+            HttpEntity responseEntity = resultResponse.getEntity();
+
+            if (responseEntity != null)
+            {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(responseEntity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("Text recognition result response: \n");
+                System.out.println(json.toString());
+
+                System.out.println("hehehehehehe");
+
+                JSONArray data = json.getJSONObject("recognitionResult").getJSONArray("lines");
+                System.out.println(data);
+                System.out.println("yang ke 2");
+
+                System.out.println("Length: " + data.length());
+                for (int i = 0; i<data.length(); i++){
+                   String getData = json.getJSONObject("recognitionResult").getJSONArray("lines").getJSONObject(i).getString("text");
+                   System.out.println(getData);
+                }
+
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
