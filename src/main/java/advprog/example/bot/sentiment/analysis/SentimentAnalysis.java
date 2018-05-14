0feabb650d28a419f8cd4cc1cbbca5ba9af30bf0
arielmikiabraham <arielabraham82@gmail.com>
2018-05-14 10:22:39 +0700

    Add API Key and Set Header

--- src/main/java/advprog/example/bot/sentiment/analysis/SentimentAnalysis.java
+++ src/main/java/advprog/example/bot/sentiment/analysis/SentimentAnalysis.java
@@ -9,27 +9,31 @@ import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 public class SentimentAnalysis {
-    private final static String AZURE_API_URL = "";
+    private static final String AZURE_API_URL =
+            "https://southeastasia.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
+    private static final String API_KEY =
+            "28cf1f357a4c4af88f7e47dcee5dc7bc";
 
     public static TextMessage analyzeText(String text) {
         String requestJson = String.format("{\"documents\":[{\n"
                 + "      \"language\": \"en\",\n"
                 + "      \"id\": \"1\",\n"
                 + "      \"text\": \"%s\"\n"
-                + "    }]}",text);
+                + "    }]}", text);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Ocp-Apim-Subscription-Key", API_KEY);
         HttpEntity<String> entity = new HttpEntity<String>(requestJson, headers);
         RestTemplate restTemplate = new RestTemplate();
 
         String response = restTemplate.postForObject(AZURE_API_URL, entity, String.class);
         JSONObject jsonObject = new JSONObject(response);
-        String result = jsonObject
+        Double result = jsonObject
                 .getJSONArray("documents")
                 .getJSONObject(0)
-                .getString("score");
+                .getDouble("score");
 
-        return new TextMessage(result);
+        return new TextMessage(String.format("%.2g", result));
     }
 }
