ff1709a6cb490056419b9f39a73cbe5a84a05609
Kelly William <kellywillliam@gmail.com>
2018-05-14 22:24:05 +0700

    Finishing the implementation of getting Top100Chart data from billboard + a minor change on its test

--- src/main/java/advprog/hot/Top100Chart.java
+++ src/main/java/advprog/hot/Top100Chart.java
@@ -1,5 +1,6 @@
 package advprog.hot;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
@@ -16,9 +17,22 @@ public class Top100Chart {
         top100 = new ArrayList<>();
     }
 
-    public void getDataFromBillboard() {
-        String targetURL = "https://www.billboard.com/charts/hot-100";
-        //get Data via JSOUP.
+    public ArrayList<SongInfo> getDataFromBillboard() {
+        String targetUrl = "https://www.billboard.com/charts/hot-100";
+        try {
+            Document document = Jsoup.connect(targetUrl).get();
+            Elements chartRows = document.getElementsByClass("chart-row");
+            for (Element chartRow : chartRows) {
+                String songArtist = chartRow.getElementsByClass("chart-row__song").html();
+                String songTitle = chartRow.getElementsByClass("chart-row__artist").html();
+                String currentRank = chartRow.getElementsByClass("chart-row__current-week").html();
+                int rank = Integer.parseInt(currentRank);
+                top100.add(new SongInfo(songArtist, songTitle, rank));
+            }
+        } catch (IOException e) {
+            e.getMessage();
+        }
+        return top100;
     }
-
 }
+
