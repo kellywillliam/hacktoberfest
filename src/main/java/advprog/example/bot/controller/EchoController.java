427382b67cc83f18200fba524dc4c6a6d2e5e954
Kelly William <kellywillliam@gmail.com>
2018-05-14 22:40:01 +0700

    Finished Line BOT Main + Test, now available to check top 10 songs from top 100 chart

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -1,5 +1,7 @@
 package advprog.example.bot.controller;
 
+import advprog.hot.SongInfo;
+import advprog.hot.Top100Chart;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
@@ -7,6 +9,7 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 @LineMessageHandler
@@ -20,10 +23,61 @@ public class EchoController {
                 event.getTimestamp(), event.getMessage()));
         TextMessageContent content = event.getMessage();
         String contentText = content.getText();
+        String[] replyText = contentText.split(" ");
 
-        String replyText = contentText.replace("/echo", "");
-        return new TextMessage(replyText.substring(1));
+        if (replyText[0].equalsIgnoreCase("/echo")) {
+            String replyEchoText = contentText.replace("/echo","");
+            return new TextMessage(replyEchoText.substring(1));
+        }
+
+        else if (replyText[0].equalsIgnoreCase("/billboard")
+                && replyText[1].equalsIgnoreCase("hot100")) {
+            Top100Chart top100Chart = new Top100Chart();
+            ArrayList<SongInfo> top100 = top100Chart.getDataFromBillboard();
+            ArrayList<SongInfo> listLagu = new ArrayList<>();
+            String artistName = replyText[2];
+            String replyBillboardText = "";
+
+            for (int i = 0; i < top100.size(); i++) {
+                if(artistName.equalsIgnoreCase(top100.get(i).getSongArtist())) {
+                    listLagu.add(top100.get(i));
+                }
+            }
+
+            if (listLagu.size() == 0) {
+                return new TextMessage("Artist " + artistName + " tidak terdapat dalam billboard");
+            }
+
+            for (int j = 0; j < listLagu.size(); j++) {
+                replyBillboardText += artistName
+                        + ("\n" + listLagu.get(j).getSongTitle() + "\n"
+                        + listLagu.get(j).getRank() + "\n\n");
+            }
+
+            return new TextMessage(replyBillboardText);
+        }
+
+        else if (replyText[0].equalsIgnoreCase("/billboard")
+                && replyText[1].equalsIgnoreCase("hot100")
+                && replyText.length==2) {
+            Top100Chart top100Chart = new Top100Chart();
+            ArrayList<SongInfo> top100 = top100Chart.getDataFromBillboard();
+            String replyTopTenBillboardText = "";
+
+            for(int i = 0; i < 10; i++) {
+                replyTopTenBillboardText += top100.get(i).getSongArtist()
+                        + ("\n" + top100.get(i).getSongArtist() + "\n"
+                        + top100.get(i).getRank() + "\n\n");
+            }
+
+            return new TextMessage(replyTopTenBillboardText);
+
+        }
+        else {
+            return new TextMessage("input tidak dapat dibaca");
+        }
     }
+
 
     @EventMapping
     public void handleDefaultMessage(Event event) {
