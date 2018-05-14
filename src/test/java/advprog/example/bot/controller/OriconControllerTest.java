2778e254cf766fdbe4892f3937ca26d3ccc081c0
Joseph J <josjov40@gmail.com>
2018-05-14 20:22:45 +0700

    fix

--- src/test/java/advprog/example/bot/controller/OriconControllerTest.java
+++ src/test/java/advprog/example/bot/controller/OriconControllerTest.java
@@ -1,4 +1,4 @@
-package advprog.example.bot;
+package advprog.example.bot.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
