e701a023ef0ed47777e4945df36f2294a5c21213
Ignatius <ignatusrahardi@gmail.com>
2018-05-14 19:27:29 +0700

    trying to handle picture messages #14

--- src/main/java/advprog/example/bot/controller/SfwCheckerController.java
+++ src/main/java/advprog/example/bot/controller/SfwCheckerController.java
@@ -1,10 +1,12 @@
 package advprog.example.bot.controller;
 
-//import org.json.*;
+
 import advprog.example.bot.confidence.percentage.ConfidencePercentage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
@@ -15,7 +17,7 @@ import java.util.logging.Logger;
 public class SfwCheckerController {
 
     private static final Logger LOGGER = Logger.getLogger(SfwCheckerController.class.getName());
-
+    private static String img = "";
 
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event)
@@ -38,6 +40,11 @@ public class SfwCheckerController {
                 break;
         }
         return new TextMessage(replyText);
+    }
+
+    @EventMapping
+    public Message handleImageMessage (MessageEvent <ImageMessageContent> event) {
+        return  new TextMessage ( "Selanjutnya cek SFW/NSFW dengan ketik command /is_sfw" );
     }
 
     @EventMapping
