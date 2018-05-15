26c83214de8eaa8facccfd1c24a96364bfeb68d7
Ignatius <ignatusrahardi@gmail.com>
2018-05-16 00:57:17 +0700

    Testing #14

--- src/main/java/advprog/example/bot/controller/SfwCheckerController.java
+++ src/main/java/advprog/example/bot/controller/SfwCheckerController.java
@@ -1,62 +1,39 @@
 package advprog.example.bot.controller;
 
 
-import advprog.example.bot.BotExampleApplication;
 import advprog.example.bot.confidence.percentage.ConfidencePercentage;
-import advprog.example.bot.confidence.percentage.LineImage;
 //import advprog.example.bot.line.ImgurApi;
 
-import com.google.common.io.ByteStreams;
-
-import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.client.MessageContentResponse;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.ImageMessage;
-import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 import java.io.*;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-import java.util.function.Consumer;
+import java.util.Base64;
 import java.util.logging.Logger;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-
 
 
 @LineMessageHandler
 public class SfwCheckerController {
 
     private static final Logger LOGGER = Logger.getLogger(SfwCheckerController.class.getName());
-    public static String idUpload = "";
-    public static void main(String[] args) throws Exception{
-
-        String url = "https://orig00.deviantart.net/e134/f/2012/259/2/5/one_piece_nami_smile____by_hiyori456-d5ew8m1.jpg";
-        RestTemplate restTemplate = new RestTemplate();
-        byte[] imageBytes = restTemplate.getForObject(url, byte[].class);
-        Files.write(Paths.get("src/main/resources/image.jpg"), imageBytes);
-        idUpload = LineImage.uploadImage("src/main/resources/image.jpg");
-        System.out.println(idUpload);
-    }
+    
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event){
         LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
@@ -92,7 +69,7 @@ public class SfwCheckerController {
         String id = content.getId();
         //String idUpload = LineImage.getImage(id);
         //String theStr = ConfidencePercentage.getFromUserImage(idUpload);
-        return new TextMessage(LineImage.getImage(id));
+        return new TextMessage(getImage(id));
 
     }
 
@@ -101,6 +78,74 @@ public class SfwCheckerController {
     public void handleDefaultMessage(Event event) {
         LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
                 event.getTimestamp(), event.getSource()));
+    }
+
+    public static String getImage(String id) throws Exception {
+        String url = "https://api.line.me/v2/bot/message/"+ id + "/content";
+        RestTemplate restTemplate = new RestTemplate();
+        byte[] imageBytes = restTemplate.getForObject(url, byte[].class);
+        Files.write(Paths.get("src/main/resources/image.jpg"), imageBytes);
+        String idImage = uploadImage("src/main/resources/image.jpg");
+        return idImage;
+    }
+
+    public static String uploadImage(String path) throws Exception{
+        /* Api calling */
+
+        String credentialsToEncode = "acc_7131bd91f718dd6" + ":" + "0438f48b7ba34d253d4df8f7e52485af";
+        String basicAuth = Base64.getEncoder().encodeToString(credentialsToEncode.getBytes(StandardCharsets.UTF_8));
+
+        URL urlObject = new URL("https://api.imagga.com/v1/content");
+        HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
+        connection.setRequestProperty("Authorization", "Basic " + basicAuth);
+
+        String boundaryString = "-Image Upload-";
+        String filepath = path;
+        File fileToUpload = new File(filepath);
+
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.addRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundaryString);
+
+        OutputStream outputStreamToRequestBody = connection.getOutputStream();
+        BufferedWriter httpRequestBodyWriter =
+                new BufferedWriter(new OutputStreamWriter(outputStreamToRequestBody));
+
+        httpRequestBodyWriter.write("\n--" + boundaryString + "\n");
+        httpRequestBodyWriter.write("Content-Disposition: form-data;"
+                + "name=\"myFile\";"
+                + "filename=\""+ fileToUpload.getName() +"\""
+                + "\nContent-Type: text/plain\n\n");
+        httpRequestBodyWriter.flush();
+
+        FileInputStream inputStreamToLogFile = new FileInputStream(fileToUpload);
+
+        int bytesRead;
+        byte[] dataBuffer = new byte[1024];
+        while((bytesRead = inputStreamToLogFile.read(dataBuffer)) != -1) {
+            outputStreamToRequestBody.write(dataBuffer, 0, bytesRead);
+        }
+
+        outputStreamToRequestBody.flush();
+
+        httpRequestBodyWriter.write("\n--" + boundaryString + "--\n");
+        httpRequestBodyWriter.flush();
+
+        outputStreamToRequestBody.close();
+        httpRequestBodyWriter.close();
+
+        BufferedReader connectionInput = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String jsonResponse = connectionInput.readLine();
+
+        connectionInput.close();
+
+        //System.out.println(jsonResponse);
+        JSONObject bigObj = new JSONObject(jsonResponse);
+        System.out.println(jsonResponse);
+        JSONArray parentArr = bigObj.getJSONArray("uploaded");
+        JSONObject smallObj = parentArr.getJSONObject(0);
+        return smallObj.getString("id");
     }
 
 
