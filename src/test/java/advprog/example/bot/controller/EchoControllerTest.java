44f86938302daab755e12999844908146a5651b3
Kezia Irene <irenekezia98@gmail.com>
2018-05-16 14:53:42 +0700

    fix test

--- src/test/java/advprog/example/bot/controller/EchoControllerTest.java
+++ src/test/java/advprog/example/bot/controller/EchoControllerTest.java
@@ -1,10 +1,12 @@
 package advprog.example.bot.controller;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.internal.verification.VerificationModeFactory.atLeastOnce;
 
 import advprog.example.bot.EventTestUtil;
 
@@ -15,6 +17,7 @@ import com.linecorp.bot.model.message.TextMessage;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -44,6 +47,35 @@ public class EchoControllerTest {
         TextMessage reply = echoController.handleTextMessageEvent(event);
 
         assertEquals("Lorem Ipsum", reply.getText());
+    }
+    
+    @Test
+    void testHandleTextMessageEvent3() {
+        MessageEvent<TextMessageContent> event3 =
+                EventTestUtil.createDummyTextMessage("asdfhalsdfjalshf");
+
+        TextMessage reply = echoController.handleTextMessageEvent(event3);
+
+        assertEquals("input tidak dapat dibaca", reply.getText());
+    }
+
+    @Test
+    void testHandleTextMessageEvent4() {
+        MessageEvent<TextMessageContent> event4 =
+                EventTestUtil.createDummyTextMessage("/billboard hotCountry");
+
+        TextMessage reply = echoController.handleTextMessageEvent(event4);
+
+        assertTrue(reply.getText().contains("(1)"));
+        assertTrue(reply.getText().contains("(2)"));
+        assertTrue(reply.getText().contains("(3)"));
+        assertTrue(reply.getText().contains("(4)"));
+        assertTrue(reply.getText().contains("(5)"));
+        assertTrue(reply.getText().contains("(6)"));
+        assertTrue(reply.getText().contains("(7)"));
+        assertTrue(reply.getText().contains("(8)"));
+        assertTrue(reply.getText().contains("(9)"));
+        assertTrue(reply.getText().contains("(10)"));
     }
 
     @Test
