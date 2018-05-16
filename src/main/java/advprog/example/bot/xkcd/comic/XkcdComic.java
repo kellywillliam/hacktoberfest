3bff76b93878f4973fd22adcc9b5278e0e48b67a
arielmikiabraham <arielabraham82@gmail.com>
2018-05-16 22:55:18 +0700

    Implement fetchComicImageUrl method in XkcdComic class
    This method can fetch image url from XkcdComic with given comic id

--- src/main/java/advprog/example/bot/xkcd/comic/XkcdComic.java
+++ src/main/java/advprog/example/bot/xkcd/comic/XkcdComic.java
@@ -1,14 +1,32 @@
 package advprog.example.bot.xkcd.comic;
 
 import com.linecorp.bot.model.message.Message;
+import org.json.JSONObject;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 public class XkcdComic {
+    private static final String XKCD_JSON_URL = "https://xkcd.com/%d/info.0.json";
 
     public static Message getComic(String input) {
         return null;
     }
 
-    private static String fetchComicImageUrl(int id) {
-        return null;
+    private static String fetchComicImageUrl(int id) throws HttpClientErrorException {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                String.format(XKCD_JSON_URL, id),
+                HttpMethod.GET,
+                null,
+                String.class);
+        try {
+            JSONObject jsonObject = new JSONObject(responseEntity.getBody());
+            return jsonObject.getString("img");
+        } catch (Exception e) {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        }
     }
 }
