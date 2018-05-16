26a80578e77f8aab89cea5eb6f63f7291efbaac0
Hilya Auli Fesmia <hilyafesmia@gmail.com>
2018-05-16 11:05:17 +0700

    fix checkStyle issue 2

--- src/main/java/advprog/example/bot/controller/JapanHot100.java
+++ src/main/java/advprog/example/bot/controller/JapanHot100.java
@@ -18,31 +18,36 @@ import org.jsoup.select.Elements;
 @LineMessageHandler
 public class JapanHot100 {
     private static final Logger LOGGER = Logger.getLogger(BotExampleApplication.class.getName());
+
     @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException {
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event)
+            throws IOException {
         TextMessageContent content = event.getMessage();
         String contentText = content.getText();
         ArrayList<String> arrArtist = new ArrayList<>();
         ArrayList<String> arrTitle = new ArrayList<>();
+
         if (contentText.equalsIgnoreCase("/japan100")) {
             String url = "https://www.billboard.com/charts/japan-hot-100";
             Document doc = Jsoup.connect(url).get();
             Elements artist = doc.getElementsByClass("chart-row__artist");
             Elements title = doc.getElementsByClass("chart-row__song");
-            for (int i = 0; i < 10; i++) {
+
+            for (int i = 0; i < 10; i++ ) {
                 Element e = artist.get(i);
                 arrArtist.add(e.text());
             }
-            for (int i = 0; i < 10; i++) {
+
+            for (int i = 0; i < 10; i++ ) {
                 Element e = title.get(i);
                 arrTitle.add(e.text());
             }
         }
         String replyText = "";
-        for (int i = 0; i < arrArtist.size(); i++) {
-            replyText = replyText+("("+(i+1)+") "+arrArtist.get(i)+" - "+arrTitle.get(i))+"\n";
+        for (int i = 0; i < arrArtist.size(); i++ ) {
+            replyText = replyText + ("(" + (i+1) + ") " + arrArtist.get(i) + " - " + arrTitle.get(i)) + "\n";
         }
-        replyText = replyText+"\nThank you for using our service";
+        replyText = replyText + "\nThank you for using our service";
         return new TextMessage(replyText);
     }
 }
