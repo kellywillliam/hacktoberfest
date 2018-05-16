c7c6ef8eea0a0951628057428d7de8043465f23a
Hilya Auli Fesmia <hilyafesmia@gmail.com>
2018-05-16 12:42:23 +0700

    fix stle

--- src/test/java/advprog/example/bot/controller/JapanHot100Test.java
+++ src/test/java/advprog/example/bot/controller/JapanHot100Test.java
@@ -1,7 +1,13 @@
 package advprog.example.bot.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import advprog.example.bot.EventTestUtil;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +31,28 @@ public class JapanHot100Test {
     void testContextLoads() {
         assertNotNull(japanHot100);
     }
+
+    @Test
+    void testHandleTextMessageEvent() throws IOException {
+        MessageEvent<TextMessageContent> event =
+                EventTestUtil.createDummyTextMessage("/japan100");
+
+        TextMessage reply = japanHot100.handleTextMessageEvent(event);
+
+        assertEquals("(1) HKT48 - Hayaokuri Calendar\n"
+                + "(2) Nogizaka46 - Syncronicity\n"
+                + "(3) SingTuyo - KISS is my life.\n"
+                + "(4) Kenshi Yonezu - Lemon\n"
+                + "(5) UVERworld - Odd Future\n"
+                + "(6) Aiko - Straw\n"
+                + "(7) Kazumasa Oda - Kono Michi Wo\n"
+                + "(8) Keyakizaka46 - Glass Wo Ware!\n"
+                + "(9) TWICE - What Is Love?\n"
+                + "(10) Hikaru Utada - Play A Love Song\n"
+                + "\n"
+                + "Thank you for using our service", reply.getText());
+    }
+
+
 
 }
