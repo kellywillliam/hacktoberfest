cd4f954317c599933e05dec691bb84e62768ccf4
arielmikiabraham <arielabraham82@gmail.com>
2018-05-16 23:03:03 +0700

    Implement getComic method in XkcdComic class
    - This method is for getting ImageMessage object if the comic id is valid
    - Otherwise, this method will return TextMessage object

--- src/main/java/advprog/example/bot/xkcd/comic/XkcdComic.java
+++ src/main/java/advprog/example/bot/xkcd/comic/XkcdComic.java
@@ -1,6 +1,8 @@
 package advprog.example.bot.xkcd.comic;
 
+import com.linecorp.bot.model.message.ImageMessage;
 import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
 import org.json.JSONObject;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -10,9 +12,16 @@ import org.springframework.web.client.RestTemplate;
 
 public class XkcdComic {
     private static final String XKCD_JSON_URL = "https://xkcd.com/%d/info.0.json";
+    private static final String INVALID_MESSAGE = "Comic ID is invalid :(";
 
     public static Message getComic(String input) {
-        return null;
+        try {
+            int id = Integer.parseInt(input);
+            String comicUrl = fetchComicImageUrl(id);
+            return new ImageMessage(comicUrl, comicUrl);
+        } catch (Exception e) {
+            return new TextMessage(INVALID_MESSAGE);
+        }
     }
 
     private static String fetchComicImageUrl(int id) throws HttpClientErrorException {
