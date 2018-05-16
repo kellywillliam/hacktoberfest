e12b3d47ecb8795b01d24db115b502ba5a837335
arielmikiabraham <arielabraham82@gmail.com>
2018-05-16 22:09:21 +0700

    Add stubs for XkcdComic class

---
+++ src/main/java/advprog/example/bot/xkcd/comic/XkcdComic.java
@@ -0,0 +1,14 @@
+package advprog.example.bot.xkcd.comic;
+
+import com.linecorp.bot.model.message.Message;
+
+public class XkcdComic {
+
+    public static Message getComic(String input) {
+        return null;
+    }
+
+    private static String fetchComicImageUrl(int id) {
+        return null;
+    }
+}
