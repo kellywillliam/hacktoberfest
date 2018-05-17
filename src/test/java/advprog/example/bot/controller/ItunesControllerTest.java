851fca95dfe9445cbbbf5d6e4c6a1f7accf8cf8a
Misael Jonathan <misaeljonathan17@gmail.com>
2018-05-18 06:19:18 +0700

    add stub and test

---
+++ src/test/java/advprog/example/bot/controller/ItunesControllerTest.java
@@ -0,0 +1,67 @@
+package advprog.example.bot.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import advprog.example.bot.EventTestUtil;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest(properties = "line.bot.handler.enabled=false")
+@ExtendWith(SpringExtension.class)
+public class ItunesControllerTest {
+
+    static {
+        System.setProperty("line.bot.channelSecret", "SECRET");
+        System.setProperty("line.bot.channelToken", "TOKEN");
+    }
+
+    @Autowired
+    private ItunesController itunesController;
+
+    @Test
+    void testContextLoads() {
+        assertNotNull(itunesController);
+    }
+
+    @Test
+    void testHandleTextMessageEvent() {
+        MessageEvent<TextMessageContent> event =
+                EventTestUtil.createDummyTextMessage("/echo Lorem Ipsum");
+
+        TextMessage reply = itunesController.handleTextMessageEvent(event);
+
+        assertEquals("Lorem Ipsum", reply.getText());
+    }
+
+    @Test
+    void testHandleDefaultMessage() {
+        Event event = mock(Event.class);
+
+        itunesController.handleDefaultMessage(event);
+
+        verify(event, atLeastOnce()).getSource();
+        verify(event, atLeastOnce()).getTimestamp();
+    }
+
+    @Test
+    void testHandleTextMessagetoAudio() {
+        Event event = null ;
+        itunesController.handleTextMessagetoAudio(event);
+    }
+
+}
