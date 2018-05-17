ca0a11a3cb6c54a8aee505e941403062570c338c
Misael Jonathan <misaeljonathan17@gmail.com>
2018-05-18 06:14:21 +0700

    add stub and test

---
+++ src/main/java/advprog/example/bot/controller/DiceController.java
@@ -0,0 +1,54 @@
+package advprog.example.bot.controller;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import java.util.logging.Logger;
+
+@LineMessageHandler
+public class DiceController
+{
+
+    private static final Logger LOGGER = Logger.getLogger(DiceController.class.getName());
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
+                event.getTimestamp(), event.getMessage()));
+        TextMessageContent content = event.getMessage();
+        String contentText = content.getText();
+
+        String replyText = contentText.replace("/echo", "");
+        return new TextMessage(replyText.substring(1));
+    }
+
+    @EventMapping
+    public void handleDefaultMessage(Event event) {
+        LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
+                event.getTimestamp(), event.getSource()));
+    }
+
+    public String diceRolling(int num) {
+        String result = "";
+        return result;
+    }
+
+    public String coinRolling(int num) {
+        String result = "";
+        return result;
+    }
+
+    public String multiRolling(int num) {
+        String result = "";
+        return result;
+    }
+
+    public String luckyNumber(int num) {
+        String result = "";
+        return result;
+    }
+}
---
+++ src/test/java/advprog/example/bot/controller/DiceControllerTest.java
@@ -0,0 +1,86 @@
+package advprog.example.bot.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import advprog.example.bot.EventTestUtil;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest(properties = "line.bot.handler.enabled=false")
+@ExtendWith(SpringExtension.class)
+public class DiceControllerTest {
+
+    static {
+        System.setProperty("line.bot.channelSecret", "SECRET");
+        System.setProperty("line.bot.channelToken", "TOKEN");
+    }
+
+    @Autowired
+    private DiceController diceController;
+
+    @Test
+    void testContextLoads() {
+        assertNotNull(diceController);
+    }
+
+    @Test
+    void testHandleTextMessageEvent() {
+        MessageEvent<TextMessageContent> event =
+                EventTestUtil.createDummyTextMessage("/echo Lorem Ipsum");
+
+        TextMessage reply = diceController.handleTextMessageEvent(event);
+
+        assertEquals("Lorem Ipsum", reply.getText());
+    }
+
+    @Test
+    void testHandleDefaultMessage() {
+        Event event = mock(Event.class);
+
+        diceController.handleDefaultMessage(event);
+
+        verify(event, atLeastOnce()).getSource();
+        verify(event, atLeastOnce()).getTimestamp();
+    }
+
+    @Test
+    void testDiceRollOutput(){} {
+        String testOutput = "";
+        assertEquals(testOutput, diceController.diceRolling(5));
+    }
+
+    @Test
+    void testCoinOutput(){} {
+        String testOutput = "";
+        assertEquals(testOutput, diceController.coinRolling(5));
+
+    }
+
+    @Test
+    void testMultiRollOutput(){} {
+        String testOutput = "";
+        assertEquals(testOutput, diceController.multiRolling(5));
+    }
+
+    @Test
+    void testIsLuckyOutput(){} {
+        String testOutput = "";
+        assertEquals(testOutput, diceController.luckyNumber(5));
+
+    }
+}
