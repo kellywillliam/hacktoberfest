0aad96419d049a5ed821df30598b0646839339b5
arielmikiabraham <arielabraham82@gmail.com>
2018-05-17 14:07:08 +0700

    Create stubs method for bing.news package

---
+++ src/main/java/advprog/example/bot/bing/news/BingNews.java
@@ -0,0 +1,13 @@
+package advprog.example.bot.bing.news;
+
+import com.linecorp.bot.model.message.TextMessage;
+
+public class BingNews {
+    public static TextMessage getFiveNews(String input) {
+        return null;
+    }
+
+    private static String fetchNews(String query) {
+        return null;
+    }
+}
