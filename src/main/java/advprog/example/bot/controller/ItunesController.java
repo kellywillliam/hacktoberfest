34c1fed7260036544b6496681f60bd680aa8eb13
Misael Jonathan <misaeljonathan17@gmail.com>
2018-05-18 22:20:36 +0700

    practice httpconnect

--- src/main/java/advprog/example/bot/controller/ItunesController.java
+++ src/main/java/advprog/example/bot/controller/ItunesController.java
@@ -7,6 +7,13 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import javax.xml.soap.Text;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.logging.Logger;
 
 @LineMessageHandler
@@ -15,14 +22,20 @@ public class ItunesController {
     private static final Logger LOGGER = Logger.getLogger(ItunesController.class.getName());
 
     @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException {
         LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
                 event.getTimestamp(), event.getMessage()));
         TextMessageContent content = event.getMessage();
         String contentText = content.getText();
+
         if (contentText.equalsIgnoreCase("hehe")) {
             return new TextMessage("bacot");
         }
+
+        if (contentText.equalsIgnoreCase("check")) {
+            return new TextMessage(connectApi());
+        }
+
         String replyText = contentText.replace("/echo", "");
         return new TextMessage(replyText.substring(1));
     }
@@ -38,6 +51,38 @@ public class ItunesController {
         LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
                 event.getTimestamp(), event.getSource()));
     }
+
+//    public static SearchResults search(SearchParameters params) {
+//        URL url;
+//        url = createUrl(searchUrl, buildSearchStringParams(params));
+//        HttpURLConnection connection = openConnection(url);
+//        return parseResponseData(readResponse(connection));
+//    }
+
+    public static String connectApi() throws IOException {
+        URL url = new URL("https://itunes.apple.com/search?term=jack+johnson");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        System.out.println(response.toString());
+        return response.toString();
+    }
+
 
 
 }
