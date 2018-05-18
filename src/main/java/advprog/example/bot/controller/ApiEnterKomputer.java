42d4e02cda93de771482ab32c25615763791846b
kevinprakasa <kevinprakasa03@gmail.com>
2018-05-18 20:17:02 +0700

    write stub for apienterkomputer

---
+++ src/main/java/advprog/example/bot/controller/ApiEnterKomputer.java
@@ -0,0 +1,7 @@
+package advprog.example.bot.controller;
+
+public class ApiEnterKomputer {
+    public String getDetailItemByCategoryAndName(String category, String name) {
+        return "detail item by category and name";
+    }
+}
