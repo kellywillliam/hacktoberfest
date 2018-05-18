08bbe929cf9edbe36eb8f4190aa119c8201520b5
yogi.lesmana <lowell.sianipar@gmail.com>
2018-05-18 10:35:44 +0700

    Add FakeNewsController class

---
+++ src/main/java/advprog/example/bot/controller/FakeNewsController.java
@@ -0,0 +1,40 @@
+package advprog.example.bot.controller;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+@LineMessageHandler
+public class FakeNewsController {
+
+    private static final Logger LOGGER = Logger.getLogger(FakeNewsController.class.getName());
+	private static UrlDatabase newsDb = new UrlDatabase();
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
+                event.getTimestamp(), event.getMessage()));
+        TextMessageContent content = event.getMessage();
+        String contentText = content.getText().trim();
+		String replyText = "";
+		//TODO check input
+        return new TextMessage(replyText);
+    }
+
+    @EventMapping
+    public void handleDefaultMessage(Event event) {
+        LOGGER.fine(String.format("Event(timestamp='%s',source='%s')",
+                event.getTimestamp(), event.getSource()));
+    }
+
+    public String validateInput(String[] inputs) {
+        //TODO validate input
+    }
+
+}
