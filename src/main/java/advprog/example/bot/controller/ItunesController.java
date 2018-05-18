02a5f1e7a92d17c77390286fb0c4daac7525c0cc
Misael Jonathan <misaeljonathan17@gmail.com>
2018-05-18 22:44:03 +0700

    practice httpconnect

--- src/main/java/advprog/example/bot/controller/ItunesController.java
+++ src/main/java/advprog/example/bot/controller/ItunesController.java
@@ -1,11 +1,15 @@
 package advprog.example.bot.controller;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.xml.soap.Text;
 import java.io.BufferedReader;
@@ -14,12 +18,22 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
 @LineMessageHandler
 public class ItunesController {
 
     private static final Logger LOGGER = Logger.getLogger(ItunesController.class.getName());
+    private static final String channelToken
+            = "KSmqRwScpX3fa90zQNt2MDrmtpXwNTGtVy"
+            + "q2K0Jyi2/gfuMSO4cSEaPBkozhqSBrJDIcDJg/"
+            + "XrcVzmyrTTm7omrS1EcBpXLb/vpKckm1HUQIQAItWE"
+            + "ZL3og1u7C2G+tK2c4TlJ+Xw0Dd"
+            + "/EpkFh1hsQdB04t89/1O/w1cDnyilFU=";
+
+    @Autowired
+    public LineMessagingClient lineMessagingClient;
 
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException {
@@ -33,12 +47,9 @@ public class ItunesController {
         }
 
         if (contentText.equalsIgnoreCase("check")) {
-            String check = connectApi();
-            if(check == null){
-                return new TextMessage("fail");
-            } else {
-                return new TextMessage(connectApi());
-            }
+            String check = connectApi(event.getReplyToken());
+            replyText(event.getReplyToken(), "wadow fail");
+            return new TextMessage(check);
         }
 
         String replyText = contentText.replace("/echo", "");
@@ -64,7 +75,7 @@ public class ItunesController {
 //        return parseResponseData(readResponse(connection));
 //    }
 
-    public static String connectApi() throws IOException {
+    public static String connectApi(String replyToken) throws IOException {
         URL url = new URL("https://itunes.apple.com/search?term=jack+johnson");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         // optional default is GET
@@ -83,10 +94,29 @@ public class ItunesController {
             response.append(inputLine);
         }
         in.close();
-
         System.out.println(response.toString());
         return response.toString();
     }
+
+    public void replyText(String replyToken, String result) {
+        lineMessagingClient = LineMessagingClient
+                .builder(channelToken)
+                .build();
+
+        final TextMessage textMessage = new TextMessage(result);
+        final ReplyMessage replyMessage = new ReplyMessage(
+                replyToken,
+                textMessage);
+
+        final BotApiResponse botApiResponse;
+        try {
+            botApiResponse = lineMessagingClient.replyMessage(replyMessage).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
 
 
 
