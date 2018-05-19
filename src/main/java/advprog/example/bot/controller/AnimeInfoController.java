63451e8830a05dc5d0d1ccb3abf6161b6a007225
Ignatius <ignatusrahardi@gmail.com>
2018-05-19 22:25:23 +0700

    testing private chat functions #50

--- src/main/java/advprog/example/bot/controller/AnimeInfoController.java
+++ src/main/java/advprog/example/bot/controller/AnimeInfoController.java
@@ -16,6 +16,27 @@ public class AnimeInfoController {
 
     private static final Logger LOGGER = Logger.getLogger(AnimeInfoController.class.getName());
 
+    public static void main(String[] args){
+        String lala = "/is_airing fullmetal alchemist";
+        String[] chatText = lala.split(" ");
+        String animeName = "";
+        String replyText = "";
+        if(chatText.length == 2){
+            animeName = chatText[1];
+            replyText = ScrapeMethod.getAnime(animeName);
+            System.out.println(ScrapeMethod.getAnime(replyText));
+        }else
+            for (int i = 1 ; i < chatText.length; i++){
+                if ( chatText.length - 1 == i){
+                    animeName += chatText[i];
+                } else {
+                    animeName += chatText[i] + " ";
+                }
+            }
+            replyText = animeName;
+        System.out.println(ScrapeMethod.getAnime(replyText));
+    }
+
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
@@ -27,6 +48,22 @@ public class AnimeInfoController {
             String[] chatText = contentText.split(" ");
             switch (chatText[0].toLowerCase()) {
                 case "/is_airing":
+                    String animeName = "";
+                    if(chatText.length == 2){
+                        animeName = chatText[1];
+                        replyText = ScrapeMethod.getAnime(animeName);
+                        return new TextMessage(replyText);
+                    } else {
+                        for (int i = 1 ; i < chatText.length; i++){
+                            if ( chatText.length - 1 == i){
+                                animeName += chatText[i];
+                            } else {
+                                animeName += chatText[i] + " ";
+                            }
+                        }
+                        replyText = ScrapeMethod.getAnime(animeName);
+                        return new TextMessage(replyText);
+                    }
                     break;
                 case "/echo":
                     replyText = contentText.replace("/echo", "")
--- src/main/java/advprog/example/bot/method/ScrapeMethod.java
+++ src/main/java/advprog/example/bot/method/ScrapeMethod.java
@@ -1,10 +1,20 @@
 package advprog.example.bot.method;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.concurrent.Exchanger;
+
+import static javax.xml.bind.DatatypeConverter.printBase64Binary;
 
 
 public class ScrapeMethod {
@@ -43,8 +53,42 @@ public class ScrapeMethod {
 
     }
 
-    public static String scrapePrivate() {
-        return null;
+
+
+    public static String getAnime(String anime) {
+        String plainCreds = "smactavish321:mactavish321";
+        String basicAuth = "Basic " + printBase64Binary(plainCreds.getBytes());
+        final String endpoint_url = "https://myanimelist.net/api/anime/search.xml?q=";
+        final String anime_url = anime;
+        String url = endpoint_url + anime_url;
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization",basicAuth);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        if (response.getBody() == null) {
+            return "anime cannot be fouund weebo";
+        } else {
+            JSONObject xmlJSONObj = XML.toJSONObject(response.getBody());
+            //String jsonPrettyPrintString = xmlJSONObj.toString(4);
+            //return jsonPrettyPrintString;
+            JSONArray entryArr = xmlJSONObj.getJSONObject("anime")
+                    .getJSONArray("entry");
+            JSONObject animeObj = entryArr.getJSONObject(0);
+            String title = animeObj.getString("title");
+            String status = animeObj.getString("status");
+            String startDate = animeObj.getString("start_date");
+            String endDate = animeObj.getString("end_date");
+            if(status.equalsIgnoreCase("Currently Airing")){
+                return title + " is airing from " + startDate + " until " + endDate;
+            }else
+            if(status.equalsIgnoreCase("Not yet aired")){
+                return title + " will air starting at " + startDate;
+            }else
+                return title + " has finished airing at " + endDate;
+        }
+
     }
 
 }
