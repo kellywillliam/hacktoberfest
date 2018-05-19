120e51bcef8a4400ba8ccc7e35373d32cb07f6df
Ignatius <ignatusrahardi@gmail.com>
2018-05-19 12:41:50 +0700

    Testing difference between private chat and group chat for bot #50

--- src/main/java/advprog/example/bot/controller/AnimeInfoController.java
+++ src/main/java/advprog/example/bot/controller/AnimeInfoController.java
@@ -3,10 +3,11 @@ package advprog.example.bot.controller;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.source.UserSource;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-import org.json.JSONObject;
+
 
 import java.util.logging.Logger;
 
@@ -20,13 +21,17 @@ public class AnimeInfoController {
         LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
                 event.getTimestamp(), event.getMessage()));
         TextMessageContent content = event.getMessage();
-        String type = event.getSource().toString();
+
         String contentText = content.getText();
         String replyText = "";
         String[] chatText = contentText.split(" ");
         switch (chatText[0].toLowerCase()) {
             case "/is_airing":
-                replyText = type;
+                if (event.getSource() instanceof UserSource) {
+                    replyText = "ini grup";
+                } else {
+                    replyText = "ini private";
+                }
                 break;
             case "/echo":
                 replyText = contentText.replace("/echo", "")
