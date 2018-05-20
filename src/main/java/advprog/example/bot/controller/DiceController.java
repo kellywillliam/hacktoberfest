4e101687055ad62077d6edae0e5cd6f57b73c27d
Misael Jonathan <misaeljonathan17@gmail.com>
2018-05-20 12:01:10 +0700

    add application.yml, add test & more function

--- src/main/java/advprog/example/bot/controller/DiceController.java
+++ src/main/java/advprog/example/bot/controller/DiceController.java
@@ -7,11 +7,16 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
 
 @LineMessageHandler
-public class DiceController
-{
+public class DiceController {
+    public static void main(String[] args) {
+        System.out.println(isLuckyNumber(5, 10, 6));
+    }
 
     private static final Logger LOGGER = Logger.getLogger(DiceController.class.getName());
 
@@ -21,6 +26,36 @@ public class DiceController
                 event.getTimestamp(), event.getMessage()));
         TextMessageContent content = event.getMessage();
         String contentText = content.getText();
+        String contentTextArr[] = contentText.split(" ");
+
+        if (contentText.equalsIgnoreCase("/coin")) {
+            String result = coinRolling();
+            return new TextMessage(result);
+        }
+
+        if (contentTextArr[0].equalsIgnoreCase("/roll")) {
+            String[] format = contentTextArr[1].split("d");
+            List<Integer> diceRoll = diceRolling(Integer.parseInt(format[0]), Integer.parseInt(format[1]));
+            String result = diceOutputBuilder(
+                    diceRoll, Integer.parseInt(format[0]), Integer.parseInt(format[1]));
+            return new TextMessage(result);
+        }
+
+        if (contentTextArr[0].equalsIgnoreCase("/multiroll")) {
+            String attempt = contentTextArr[1];
+            String[] format = contentTextArr[1].split("d");
+            String result = multiRolling(
+                    Integer.parseInt(attempt), Integer.parseInt(format[0]), Integer.parseInt(format[1]));
+            return new TextMessage(result);
+        }
+
+        if (contentTextArr[0].equalsIgnoreCase("/is_lucky")) {
+            String luckyNum = contentTextArr[1];
+            String[] format = contentTextArr[2].split("d");
+            String result = isLuckyNumber(
+                    Integer.parseInt(luckyNum), Integer.parseInt(format[0]), Integer.parseInt(format[1]));
+            return new TextMessage(result);
+        }
 
         String replyText = contentText.replace("/echo", "");
         return new TextMessage(replyText.substring(1));
@@ -32,23 +67,71 @@ public class DiceController
                 event.getTimestamp(), event.getSource()));
     }
 
-    public String diceRolling(int num) {
-        String result = "";
+    public static List<Integer> diceRolling(int x, int y) {
+        List<Integer> result = new ArrayList<Integer>();
+        for (int roll = 0; roll < x; roll++) {
+            result.add(oneRoll(x, y));
+        }
         return result;
     }
 
-    public String coinRolling(int num) {
-        String result = "";
-        return result;
+    public static Integer oneRoll(int xSided, int ySided) {
+        Random rand = new Random();
+        return rand.nextInt(ySided) + 1;
     }
 
-    public String multiRolling(int num) {
-        String result = "";
-        return result;
+
+    public static String multiRolling(int attempt, int x, int y) {
+        List<Integer> result = new ArrayList<Integer>();
+        String output = "";
+        for (int j = 0; j < attempt; j++) {
+            output += multiRollOutputBuilder(x, y, diceRolling(x, y));
+        }
+        return output;
     }
 
-    public String luckyNumber(int num) {
-        String result = "";
-        return result;
+
+    public static String multiRollOutputBuilder(int x, int y, List<Integer> subList) {
+        String base = x + "d" + y + " ";
+        String start = "(";
+        for (int i = 0; i < subList.size(); i++) {
+            start += subList.get(i) + ", ";
+        }
+        start = start.substring(0, start.length()-2) + ")\n";
+        return base + start;
+
+    }
+
+
+    public static String diceOutputBuilder(List<Integer> list, int x, int y) {
+        String base = "Result: " + x + "d" + y + " (";
+        for (int i = 0; i < x; i++) {
+            base += list.get(i) + ", ";
+        }
+        String output = base.substring(0, base.length()-2) + ")";
+        return output;
+    }
+
+    public static  String coinRolling() {
+        Random rand = new Random();
+        int n = rand.nextInt(2);
+        if (n == 0) {
+            return "head";
+        } else {
+            return "tail";
+        }
+    }
+
+    public static String isLuckyNumber(int luckyNum, int x, int y) {
+        List<Integer> possibilities = diceRolling(x, y);
+        List<Integer> luck = new ArrayList<Integer>();
+        luck.add(luckyNum);
+        possibilities.retainAll(luck);
+
+        if (possibilities.size() == 0) {
+            return "I'm not lucky";
+        } else {
+            return luckyNum + " appears " + possibilities.size();
+        }
     }
 }
