e0a0eb0bca493383d8f064848e8a83ee234533c1
Joseph J <josjov40@gmail.com>
2018-05-20 14:06:42 +0700

    aha

--- src/main/java/advprog/example/bot/controller/HangoutController.java
+++ src/main/java/advprog/example/bot/controller/HangoutController.java
@@ -25,7 +25,7 @@ public class HangoutController {
         String contentText = content.getText();
         
         String replyText = contentText.replace("/Hangout", "");
-        return new TextMessage(replyText.substring(1));
+        return new TextMessage(replyText);
     }
 
     @EventMapping
