5fbd1c4f8646a20535f94c8f5525a01a6f52af34
ivanairene <ivanaairenee@gmail.com>
2018-05-21 19:13:30 +0700

    comment out echo to test function

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -14,16 +14,16 @@ public class EchoController {
 
     private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
 
-    @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
-                event.getTimestamp(), event.getMessage()));
-        TextMessageContent content = event.getMessage();
-        String contentText = content.getText();
+    // @EventMapping
+    // public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+    //    LOGGER.fine(String.format("TextMessageContent(timestamp='%s',content='%s')",
+    //            event.getTimestamp(), event.getMessage()));
+    //    TextMessageContent content = event.getMessage();
+    //    String contentText = content.getText();
 
-        String replyText = contentText.replace("/echo", "");
-        return new TextMessage(replyText.substring(1));
-    }
+    //    String replyText = contentText.replace("/echo", "");
+    //    return new TextMessage(replyText.substring(1));
+    // }
 
     @EventMapping
     public void handleDefaultMessage(Event event) {
