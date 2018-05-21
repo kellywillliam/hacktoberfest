e8bdccb49a0c582a68cadfbe61b7691eacff16ef
Hilya Auli Fesmia <hilyafesmia@gmail.com>
2018-05-22 01:00:36 +0700

    keep random trivia

--- src/main/java/advprog/example/bot/composer/RandomTriviaComposer.java
+++ src/main/java/advprog/example/bot/composer/RandomTriviaComposer.java
@@ -1,14 +1,8 @@
 package advprog.example.bot.composer;
 
-import advprog.example.bot.composer.RandomTriviaComposer;
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.action.Action;
 import com.linecorp.bot.model.message.template.CarouselColumn;
 import com.linecorp.bot.model.message.template.CarouselTemplate;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -18,19 +12,25 @@ public class RandomTriviaComposer {
 
     public RandomTriviaComposer() {}
 
-    public CarouselTemplate addQuestion(String question, String answer) {
-//        String[] answers = answer.split("\n");
-//        List<Action> actionList = new LinkedList<>();
+    public void addQuestion(String question) {
+
+
+    }
+
+    public CarouselTemplate addAnswers(String answer) {
+        String[] answers = answer.split("\n");
+
+        List<Action> actionList = new LinkedList<>();
         List<CarouselColumn> carouselColumns = new LinkedList<>();
-//
+
         CarouselTemplate carouselTemplate = new CarouselTemplate(carouselColumns);
-//
-//        for (String choice: answers) {
-//            CarouselColumn card = new CarouselColumn("url", choice, choice, actionList);
-//        }
-//
+
+        for (String choice: answers) {
+            CarouselColumn card = new CarouselColumn("url", choice, choice, actionList);
+        }
         return carouselTemplate;
     }
+
 
     public void changeAnswer() {
 
