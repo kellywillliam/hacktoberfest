145540805a01395b22b545dd8806068c9b401465
Diosox <claudioyosafat@gmail.com>
2018-05-22 15:06:17 +0700

    Test Deploy to Heroku

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -131,7 +131,7 @@ public class EchoController {
             String latitude = Double.toString(content.getLatitude());
             String userId = content.getId();
 
-            String dataInformation = longitude + '-' + latitude;
+            String dataInformation = longitude + ';' + latitude;
 
             replyText = eventController.getData(dataInformation, userId);
 
