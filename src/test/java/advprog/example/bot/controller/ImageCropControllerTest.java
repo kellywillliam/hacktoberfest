4f59b42954cccdbdce2f3f989abbc172f57c6938
ivanairene <ivanaairenee@gmail.com>
2018-05-22 12:29:13 +0700

    add system env on channel secret and token

--- src/test/java/advprog/example/bot/controller/ImageCropControllerTest.java
+++ src/test/java/advprog/example/bot/controller/ImageCropControllerTest.java
@@ -72,8 +72,6 @@ public class ImageCropControllerTest {
 
     @Test
     void testHandleImageDoesNotExist() throws IOException {
-        File fileToBeDeleted = new File("src/main/resources/imagecrop.jpg");
-        fileToBeDeleted.delete();
         ImageCropController imageCropController = new ImageCropController();
         String result = imageCropController.uploadContentToImgur();
         System.out.println(result);
