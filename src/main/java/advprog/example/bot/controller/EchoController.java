c8e304a1d60d735bcfde32215ad602b0efd7a511
Diosox <claudioyosafat@gmail.com>
2018-05-22 18:13:20 +0700

    Test Bot

--- src/main/java/advprog/example/bot/controller/EchoController.java
+++ src/main/java/advprog/example/bot/controller/EchoController.java
@@ -7,10 +7,6 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.logging.Logger;
@@ -18,17 +14,19 @@ import java.util.logging.Logger;
 
 @LineMessageHandler
 public class EchoController {
-    private static HashMap<String,String> userConfig = new HashMap<>();
-
     private static final Logger LOGGER = Logger.getLogger(EchoController.class.getName());
 
-    private static final String URL = "http://api.openweathermap.org/data/2.5/weather?";
-    private static final String COUNTRYURL = "https://restcountries.eu/rest/v2/alpha/";
-    private static final String APIWEATHERKEY = "&appid=e2379a68cf1e649b79bd43beff3a0407";
+    public static final int STANDARD = 0;
+    public static final int METRIC = 1;
+    public static final int IMPERIAL = 2;
+    private int state = 0;
 
     private boolean flag = false;
 
+    public
+
     TopChartController eventHandler = new TopChartController();
+    WeatherController eventController = new WeatherController();
 
     String errorMessage = "Format yang anda masukkan salah.\n"
             + "Untuk format yang benar adalah sbb :\n"
@@ -97,11 +95,14 @@ public class EchoController {
             String userId = content.getId();
             String tipe = temp[1];
             if (tipe.equalsIgnoreCase("STANDARD")) {
-                replyText = updateUserConfig(userId,"STANDARD");
+                state = STANDARD;
+                replyText = "Konfigurasi data kamu sudah di-update YEAY !";
             } else if (tipe.equalsIgnoreCase("METRIC")) {
-                replyText = updateUserConfig(userId,"METRIC");
+                state = METRIC;
+                replyText = "Konfigurasi data kamu sudah di-update YEAY !";
             } else if (tipe.equalsIgnoreCase("IMPERIAL")) {
-                replyText = updateUserConfig(userId,"IMPERIAL");
+                state = IMPERIAL;
+                replyText = "Konfigurasi data kamu sudah di-update YEAY !";
             } else {
                 replyText = "Opsi yang kamu pilih tidak tersedia :( ";
             }
@@ -139,137 +140,25 @@ public class EchoController {
             String longitude = Double.toString(content.getLongitude());
             String latitude = Double.toString(content.getLatitude());
             String userId = content.getId();
-            String tipe;
-            if(!userConfig.containsKey(userId)) {
-                userConfig.put(userId,"STANDARD");
-                tipe = userConfig.get(userId);
-            } else {
-                tipe = userConfig.get(userId);
+
+            String tipe = null;
+
+            if(state == STANDARD) {
+                tipe = "STANDARD";
+            } else if (state == METRIC) {
+                tipe = "METRIC";
+            } else if (state == IMPERIAL) {
+                tipe = "IMPERIAL";
             }
-            replyText = getData(longitude,latitude, userId,tipe);
+
+            replyText = eventController.getData(longitude,latitude, userId, tipe);
 
             flag = false;
 
-            return new TextMessage(replyText + " Hashmap: " + getUserConfig().get(userId));
+            return new TextMessage(replyText + " Hashmap: " + tipe);
         }
 
         return new TextMessage("Info yang kamu masukkan salah");
 
-    }
-
-    public HashMap<String, String> getUserConfig() {
-        return userConfig;
-    }
-
-    public String updateUserConfig(String userId, String tipe) {
-        System.out.println(userConfig.get(userId));
-        userConfig.put(userId,tipe);
-        System.out.println("WOIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
-        System.out.println(userConfig.get(userId));
-        return "Konfigurasi data kamu sudah di-update YEAY !";
-    }
-
-    public String getLocation(JSONObject json) {
-        String city = (String) json.get("name"); // Ambil nama kota
-
-        JSONObject sys = (JSONObject) json.get("sys");
-        String kodeNegara = (String) sys.get("country");
-
-        String urlNegara = COUNTRYURL + kodeNegara;
-        String jsonNegara = getJsonFromApi(urlNegara);
-
-        JSONObject jsonCountry = new JSONObject(jsonNegara);
-        String country = (String) jsonCountry.get("name");
-        return city + ", " + country;
-
-    }
-
-    public String getWeather(JSONObject json) {
-        JSONArray weather = (JSONArray) json.get("weather");
-        JSONObject weatherArr = (JSONObject) weather.get(0);
-        String hasil = (String) weatherArr.get("main");
-        return hasil;
-    }
-
-    public double getTemperature(JSONObject json) {
-        JSONObject data = (JSONObject) json.get("main");
-        double temperature = Double.parseDouble(data.get("temp").toString());
-        return temperature;
-    }
-
-    public double getWindSpeed(JSONObject json) {
-        JSONObject wind = (JSONObject) json.get("wind");
-        double windSpeed = Double.parseDouble(wind.get("speed").toString());
-        return windSpeed;
-    }
-
-    public double getHumidity(JSONObject json) {
-        JSONObject data = (JSONObject) json.get("main");
-        double humidity = Double.parseDouble(data.get("humidity").toString());
-        return humidity;
-    }
-
-
-    public String getJsonFromApi(String url) {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders header = new HttpHeaders();
-        return restTemplate.getForObject(url, String.class, header);
-    }
-
-
-
-    public String getData(String lon,String lat, String userId,String tipe) {
-        String jsonData;
-        String userUnit;
-//
-//        if (!userConfig.containsKey(userId)) {
-//            userConfig.put(userId,"STANDARD");
-//            userUnit = userConfig.get(userId);
-//        } else {
-//            userUnit = userConfig.get(userId);
-//        }
-
-        String urlApi =  URL + "lat=" + lat + "&lon="
-                + lon + "&units=" + tipe + APIWEATHERKEY;
-        jsonData = getJsonFromApi(urlApi);
-
-        JSONObject json = new JSONObject(jsonData);
-        String location = getLocation(json);
-        String weather = getWeather(json);
-        double temperature = getTemperature(json);
-        double wind = getWindSpeed(json);
-        double humidity = getHumidity(json);
-
-        String windUnit = null;
-        String tempUnit = null;
-
-        if (tipe.equalsIgnoreCase("STANDARD")) {
-            windUnit = "meter/sec";
-            tempUnit = "Kelvin";
-        } else if (tipe.equalsIgnoreCase("METRIC")) {
-            windUnit = "meter/sec";
-            tempUnit = "Celcius";
-        } else if (tipe.equalsIgnoreCase("IMPERIAL")) {
-            windUnit = "miles/hour";
-            tempUnit = "Fahrenheit";
-        }
-//        } else {
-//             windUnit = "meter/sec";
-//             tempUnit = "Kelvin";
-//         }
-
-        String result = text(location,weather,temperature,wind,humidity,windUnit,tempUnit);
-
-        return result;
-    }
-
-    public String text(String location, String weather, double temperature, double wind,
-                       double humidity, String windUnit, String tempUnit) {
-        String result = "Weather at your position (" + location + "):\n"
-                + weather + " :" + weather + ":" + "\n" + wind + " " + windUnit
-                + "\n" + temperature + " " + tempUnit + "\n"
-                + humidity + "%";
-
-        return result;
     }
 }
--- src/main/java/advprog/example/bot/controller/WeatherController.java
+++ src/main/java/advprog/example/bot/controller/WeatherController.java
@@ -1,122 +1,130 @@
-//package advprog.example.bot.controller;
-//
-//import java.util.HashMap;
-//
-//import org.json.JSONArray;
-//import org.json.JSONObject;
-//
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.web.client.RestTemplate;
-//
-//public class WeatherController {
-//
-//    private static final String URL = "http://api.openweathermap.org/data/2.5/weather?";
-//    private static final String COUNTRYURL = "https://restcountries.eu/rest/v2/alpha/";
-//    private static final String APIWEATHERKEY = "&appid=e2379a68cf1e649b79bd43beff3a0407";
-//
-//    public String getLocation(JSONObject json) {
-//        String city = (String) json.get("name"); // Ambil nama kota
-//
-//        JSONObject sys = (JSONObject) json.get("sys");
-//        String kodeNegara = (String) sys.get("country");
-//
-//        String urlNegara = COUNTRYURL + kodeNegara;
-//        String jsonNegara = getJsonFromApi(urlNegara);
-//
-//        JSONObject jsonCountry = new JSONObject(jsonNegara);
-//        String country = (String) jsonCountry.get("name");
-//        return city + ", " + country;
-//
+package advprog.example.bot.controller;
+
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+
+public class WeatherController {
+
+    private static HashMap<String,String> userConfig = new HashMap<>();
+
+    private static final String URL = "http://api.openweathermap.org/data/2.5/weather?";
+    private static final String COUNTRYURL = "https://restcountries.eu/rest/v2/alpha/";
+    private static final String APIWEATHERKEY = "&appid=e2379a68cf1e649b79bd43beff3a0407";
+
+    public String getLocation(JSONObject json) {
+        String city = (String) json.get("name"); // Ambil nama kota
+
+        JSONObject sys = (JSONObject) json.get("sys");
+        String kodeNegara = (String) sys.get("country");
+
+        String urlNegara = COUNTRYURL + kodeNegara;
+        String jsonNegara = getJsonFromApi(urlNegara);
+
+        JSONObject jsonCountry = new JSONObject(jsonNegara);
+        String country = (String) jsonCountry.get("name");
+        return city + ", " + country;
+
+    }
+
+    public HashMap<String, String> getUserConfig() {
+        return userConfig;
+    }
+
+    public String getWeather(JSONObject json) {
+        JSONArray weather = (JSONArray) json.get("weather");
+        JSONObject weatherArr = (JSONObject) weather.get(0);
+        String hasil = (String) weatherArr.get("main");
+        return hasil;
+    }
+
+    public double getTemperature(JSONObject json) {
+        JSONObject data = (JSONObject) json.get("main");
+        double temperature = Double.parseDouble(data.get("temp").toString());
+        return temperature;
+    }
+
+    public double getWindSpeed(JSONObject json) {
+        JSONObject wind = (JSONObject) json.get("wind");
+        double windSpeed = Double.parseDouble(wind.get("speed").toString());
+        return windSpeed;
+    }
+
+    public double getHumidity(JSONObject json) {
+        JSONObject data = (JSONObject) json.get("main");
+        double humidity = Double.parseDouble(data.get("humidity").toString());
+        return humidity;
+    }
+
+
+    public String getJsonFromApi(String url) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders header = new HttpHeaders();
+        return restTemplate.getForObject(url, String.class, header);
+    }
+
+//    public String updateUserConfig(String userId, String tipe) {
+//        System.out.println(userConfig.get(userId));
+//        userConfig.put(userId,tipe);
+//        System.out.println("WOIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+//        System.out.println(userConfig.get(userId));
+//        return "Konfigurasi data kamu sudah di-update YEAY !";
 //    }
-//
-//    public String getWeather(JSONObject json) {
-//        JSONArray weather = (JSONArray) json.get("weather");
-//        JSONObject weatherArr = (JSONObject) weather.get(0);
-//        String hasil = (String) weatherArr.get("main");
-//        return hasil;
-//    }
-//
-//    public double getTemperature(JSONObject json) {
-//        JSONObject data = (JSONObject) json.get("main");
-//        double temperature = Double.parseDouble(data.get("temp").toString());
-//        return temperature;
-//    }
-//
-//    public double getWindSpeed(JSONObject json) {
-//        JSONObject wind = (JSONObject) json.get("wind");
-//        double windSpeed = Double.parseDouble(wind.get("speed").toString());
-//        return windSpeed;
-//    }
-//
-//    public double getHumidity(JSONObject json) {
-//        JSONObject data = (JSONObject) json.get("main");
-//        double humidity = Double.parseDouble(data.get("humidity").toString());
-//        return humidity;
-//    }
-//
-//
-//    public String getJsonFromApi(String url) {
-//        RestTemplate restTemplate = new RestTemplate();
-//        HttpHeaders header = new HttpHeaders();
-//        return restTemplate.getForObject(url, String.class, header);
-//    }
-//
-//
-//
-//    public String getData(String lon,String lat, String userId,String tipe) {
-//        String jsonData;
-//        String userUnit;
-////
-////        if (!userConfig.containsKey(userId)) {
-////            userConfig.put(userId,"STANDARD");
-////            userUnit = userConfig.get(userId);
-////        } else {
-////            userUnit = userConfig.get(userId);
-////        }
-//
-//        String urlApi =  URL + "lat=" + lat + "&lon="
-//                    + lon + "&units=" + tipe + APIWEATHERKEY;
-//        jsonData = getJsonFromApi(urlApi);
-//
-//        JSONObject json = new JSONObject(jsonData);
-//        String location = getLocation(json);
-//        String weather = getWeather(json);
-//        double temperature = getTemperature(json);
-//        double wind = getWindSpeed(json);
-//        double humidity = getHumidity(json);
-//
-//        String windUnit = null;
-//        String tempUnit = null;
-//
-//        if (tipe.equalsIgnoreCase("STANDARD")) {
-//            windUnit = "meter/sec";
-//            tempUnit = "Kelvin";
-//        } else if (tipe.equalsIgnoreCase("METRIC")) {
-//            windUnit = "meter/sec";
-//            tempUnit = "Celcius";
-//        } else if (tipe.equalsIgnoreCase("IMPERIAL")) {
-//            windUnit = "miles/hour";
-//            tempUnit = "Fahrenheit";
+
+    public String getData(String lon,String lat, String userId,String tipe) {
+        String jsonData;
+        String userUnit;
+
+//        if (!userConfig.containsKey(userId)) {
+//            userConfig.put(userId,"STANDARD");
+//            userUnit = userConfig.get(userId);
+//        } else {
+//            userUnit = userConfig.get(userId);
 //        }
-////        } else {
-////             windUnit = "meter/sec";
-////             tempUnit = "Kelvin";
-////         }
-//
-//        String result = text(location,weather,temperature,wind,humidity,windUnit,tempUnit);
-//
-//        return result;
-//    }
-//
-//    public String text(String location, String weather, double temperature, double wind,
-//                       double humidity, String windUnit, String tempUnit) {
-//        String result = "Weather at your position (" + location + "):\n"
-//                + weather + " :" + weather + ":" + "\n" + wind + " " + windUnit
-//                + "\n" + temperature + " " + tempUnit + "\n"
-//                + humidity + "%";
-//
-//        return result;
-//    }
-//
-//
-//}
+
+        String urlApi =  URL + "lat=" + lat + "&lon="
+                    + lon + "&units=" + tipe + APIWEATHERKEY;
+        jsonData = getJsonFromApi(urlApi);
+
+        JSONObject json = new JSONObject(jsonData);
+        String location = getLocation(json);
+        String weather = getWeather(json);
+        double temperature = getTemperature(json);
+        double wind = getWindSpeed(json);
+        double humidity = getHumidity(json);
+
+        String windUnit = null;
+        String tempUnit = null;
+
+        if (tipe.equalsIgnoreCase("STANDARD")) {
+            windUnit = "meter/sec";
+            tempUnit = "Kelvin";
+        } else if (tipe.equalsIgnoreCase("METRIC")) {
+            windUnit = "meter/sec";
+            tempUnit = "Celcius";
+        } else if (tipe.equalsIgnoreCase("IMPERIAL")) {
+            windUnit = "miles/hour";
+            tempUnit = "Fahrenheit";
+        }
+
+        String result = text(location,weather,temperature,wind,humidity,windUnit,tempUnit);
+
+        return result;
+    }
+
+    public String text(String location, String weather, double temperature, double wind,
+                       double humidity, String windUnit, String tempUnit) {
+        String result = "Weather at your position (" + location + "):\n"
+                + weather + " :" + weather + ":" + "\n" + wind + " " + windUnit
+                + "\n" + temperature + " " + tempUnit + "\n"
+                + humidity + "%";
+
+        return result;
+    }
+
+
+}
