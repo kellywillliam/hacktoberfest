ba11d15e188c2013a0397b18c52ce6a48306b3b0
Hilya Auli Fesmia <hilyafesmia@gmail.com>
2018-05-22 07:08:26 +0700

    stub

---
+++ src/test/java/advprog/example/bot/controller/YoutubeGetterTest.java
@@ -0,0 +1,12 @@
+package advprog.example.bot.controller;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class YoutubeGetterTest {
+
+    private YoutubeGetter youtubeGetter;
+
+    void getInfoYoutube(String url) {
+        assertTrue(true);
+    }
+}
